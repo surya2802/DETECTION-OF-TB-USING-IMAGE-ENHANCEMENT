7408277f27cfd79060fe2d3e5753826f13f91fff
surya2802 <suryasankar2802@gmail.com>
2022-05-25 08:16:40 +0530

    first commit

---
+++ DATASET/gui.m
@@ -0,0 +1,244 @@
+function varargout = gui(varargin)
+% GUI MATLAB code for gui.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui
+
+% Last Modified by GUIDE v2.5 28-Apr-2022 12:51:29
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before gui is made visible.
+function gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui (see VARARGIN)
+
+% Choose default command line output for gui
+handles.output = hObject;
+
+a=zeros([256 256]);
+axes(handles.axes1);
+imshow(a);
+
+axes(handles.axes2);
+imshow(a);
+
+axes(handles.axes3);
+imshow(a);
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global train 
+matlabpath ='C:\Users\karth\Desktop\tuber matlab\'
+data = fullfile(matlabpath,'dataset')
+
+train = imageDatastore(data, 'IncludeSubfolders',true,'LabelSource','foldernames');
+count = train.countEachLabel;
+
+msgbox('Dataset Loaded Successfully')
+
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes on button press in pushbutton7.
+function pushbutton7_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global layers
+disp('Pre-Trained Model Loaded...')
+net = googlenet;
+deepNetworkDesigner(net)
+layers = [ imageInputLayer([400 400 3])
+    
+net(2:end-3)
+
+fullyConnectedLayer(2)
+
+softmaxLayer
+
+classificationLayer()
+
+]
+msgbox('Pre-Trained Model Loaded Successfully')
+
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+%training
+global opt training train layers
+opt = trainingOptions('adam', ...
+    'InitialLearnRate', 0.001, ...
+    'LearnRateSchedule', 'piecewise', ...
+    'LearnRateDropFactor', 0.1, ...
+    'LearnRateDropPeriod', 8, ...
+    'L2Regularization', 0.004, ...
+    'MaxEpochs', 10, ...
+    'MiniBatchSize', 100, ...
+    'Verbose', true, 'Plots','training-progress');
+
+
+training = trainNetwork(train,layers,opt);
+
+msgbox('Trained Completed')
+
+% Update handles structure
+guidata(hObject, handles);
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global inp
+cd input
+[file path] = uigetfile('*.bmp;*.jpg;*.jpeg','Pick an Image File');
+
+
+if isequal(file,0)
+       warndlg('File not selected');
+else
+
+inp = imread(file);
+cd ..
+axes(handles.axes1);
+imshow(inp);
+img=inp;
+if size(inp,3)>1
+    Freg =im2gray(inp); 
+    
+end
+handles.img=img;
+end
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global inp J
+img = im2gray(inp);
+J = medfilt2(img);
+axes(handles.axes2);
+title('Filtered Image');
+imshow(J);
+% Update handles structure
+guidata(hObject, handles);
+% --- Executes on button press in pushbutton4.
+
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global training inp out
+out = classify(training,inp);
+axes(handles.axes3);
+imshow(inp);
+title(string(out));
+% Update handles structure
+guidata(hObject, handles);
+if (string(out)=="Normal")
+                
+  msgbox(string(out));
+else
+            
+  msgbox(string(out));
+  [segout] = seg(inp); 
+  boundary = bwboundaries(im2bw(segout));
+     
+end
+% out = classify(training,inp);
+% yval = inp;
+% accuracy = mean()
+
+
+           
---
+++ README.txt
@@ -0,0 +1,13 @@
+Step 1: open the MATLAB Software.
+Step 2: Then copy the code in code editor and  save gui.m as separate file and save seg.m as separate file.
+Step 3: Now open the tubermatlab folder which conatins the dataset and copy the whole folder(tubermatlab) to your desktop.
+Step 4: Now change the directory of input to this folder.
+Step 5: Then run the code.
+Step 6: Then the Gui Window opens.
+Step 7: And now first load the dataset using load dataset option.
+Step 8: Now click on the Load Model to load the model.
+Step 9: Now train the system using Training button and once the training is over, the testing phase starts.
+Step 10: Now in testing click on test image and upload an image.
+Step 11: After that, click on pre-process to pre-process the image.
+Step 12: once pre-processed image is shown then click Classification.
+Step 13: The output is displayed as textBox with Normal Or Tuberculosis, if Tuberculosis then segmented image is shown. 
